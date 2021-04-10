aea42f63debef318540d17e0eb08353c01da994e
momo <wdudtlrw78@gmail.com>
2021-04-11 01:56:42 +0900

    next-redux-wrapper의 HYDRATE 주석 설명 추가

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -31,6 +31,7 @@ export const logoutAction = (data) => {
 // (이전상태, 액션) => 다음상태
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    //  HYDRATE는 next-redux-wrapper를 쓸 때 필요한 액션. 서버쪽에서 실행된 리덕스의 결과물이 프론트에서는 HYDRATE라는 액션 이름 아래에 데이터로 전달된다.
     case HYDRATE:
       console.log("HYDRATE", action);
       return { ...state, ...action.payload };
