b21803b0d1783e037725ed2471eb35b924722ea2
momo <wdudtlrw78@gmail.com>
2021-04-12 21:16:39 +0900

    글로벌 스타일과 컴포넌트 폴더 구조

--- prepare/front/components/AppLayouts.js
+++ prepare/front/components/AppLayouts.js
@@ -6,10 +6,26 @@ import styled from "styled-components";
 import UserProfile from "./UserProfile";
 import LoginForm from "./LoginForm";
 import { useSelector } from "react-redux";
+import { createGlobalStyle } from "styled-components";
 
 // - xs : 모바일
 // - sm : 태블릿
 // - md : 작은 데스크탑
+
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+
+  .ant-col:first-child {
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`;
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
@@ -26,6 +42,7 @@ const AppLayouts = ({ children }) => {
 
   return (
     <div>
+      <Global />
       <Menu mode="horizontal">
         <Menu.Item>
           <Link href="/">
