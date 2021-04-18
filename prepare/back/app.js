8a2e3867ab5e1184b5ca201638b0ab020428cab8
momo <wdudtlrw78@gmail.com>
2021-04-19 00:56:51 +0900

    익스프레스로 라우팅하기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,47 +1,59 @@
-const http = require('http'); // node 에서 기본적으로 http 모듈 제공
+const express = require('express'); // node 에서 기본적으로 http 모듈 제공
 
 // node 런타임이 코드를 실행해서 http가 서버 역할을 하는거지 node 자체가 서버는 아니다.
+// express도 내부적으로 http 쓰기 때문에 서버를 돌릴 수 있다.
 
-const server = http.createServer((req, res) => {
-  // 백엔드 서버와 프론트 서버 나누는 이유
-  // 대규모 app 대비
-  // 프론트는 SSR 백엔드는 API 한 컴퓨터에 두 개를 띄어도 되지만
-  // 만약에 프론트 서버에 요청이 1초에 1000개 가 오고 백엔드는 10개로 비대칭적으로 올 때가 있다.
-  // 그러면 서버가 메모리나 CPU가 부족해서 터진다.
-  // 대비해서 스케일링을 해준다. (컴퓨터를 복사한다 기존거를 통째로)
-  // 그러면 프론트 1000 만큼을 나눠 받도록 하지만 여기서 백엔드는 불필요한 낭비가 된다.
-  // 이러한 문제로 대규모 프로젝트 같은 경우 각 기능별로 서버로 나누어주는 경우가 많다.
-  // 그래야지 그 특정 기능에 데이터 요청이 많이 왔을 때 그 기능만 서버 여러대로 늘려주면 되기 떄문에
-  // 한 컴퓨터에다가 모든 기능을 다 넣어두면 그 서버를 스케일링할 때 모든 기능이 복사되는데 그럼 나머지 것들은 쓸모없게 되버리기 때문이다.
+const app = express();
 
-  console.log(req.url, req.method);
+// app.get -> 가져오다
+// app.post -> 생성하다
+// app.put -> 전체 수정 ex) 아예 통째로 엎어 씌울떄
+// app.delete -> 제거
+// app.patch -> 부분 수정 ex) 사용자 정보가 있는데 닉네임만 변경하고 싶을 때
+// app.options -> 찔러보기
+// app.head -> 헤더만 가져요기(헤더/ 바디) (여기선 사용 X)
 
-  if (req.method === 'GET') {
-    if (req.url === '/api/posts') {
-    }
-  } else if (req.method === 'POST') {
-  } else if (req.method === 'DELETE') {
-  }
-
-  res.write('<h1>Hollo node1</h1>');
-  res.write('Hollo node2');
-  res.write('Hollo node3');
-  res.write('Hollo node4');
-  res.end('Hello node8');
-
-  // favicon.ico 는 브라우저 자체가 요청한다. (탭 아이콘)
-  // res.end는 마지막에만 쓴다.
-
-  // param req = 브라우저나 프론트서버에서 온 요청에 대한 정보
-  // param res = 응답에 대한 정보
-
-  // res.write, res.end 등 red를 사용 = 응답을 보내고 싶을 때
-  // req.url, req.method = 요청이 뭔지에 대한 정보를 얻고싶을 때
+app.get('/', (req, res) => {
+  res.send('hello express');
 });
 
-server.listen(3065, () => {
+app.get('/api', (req, res) => {
+  res.send('hello api');
+});
+
+app.get('/api/posts', (req, res) => {
+  // 여기는 서비스 따라 달라서 백엔드와 협의를 잘해야 한다.
+  // 백엔드 개발자가 어떤 데이터를 줘야되는지 협의를 하고
+  // 백엔드 개발자들은 이러한 라우터들을 만들어 놓는게 주 역할이다.
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+// post랑 delete 요청하기
+// 브라우저 주소창은 get 요청만 되고 이외의 요청들은 postman툴 사용
+app.post('/api/post', (req, res) => {
+  res.json({ id: 1, content: 'hello' });
+});
+
+app.delete('/api/post', (req, res) => {
+  res.send({ id: 1 });
+});
+
+app.listen(3065, () => {
   console.log('서버 실행 중');
 });
+
+// favicon.ico 는 브라우저 자체가 요청한다. (탭 아이콘)
+// res.end는 마지막에만 쓴다.
+
+// param req = 브라우저나 프론트서버에서 온 요청에 대한 정보
+// param res = 응답에 대한 정보
+
+// res.write, res.end 등 red를 사용 = 응답을 보내고 싶을 때
+// req.url, req.method = 요청이 뭔지에 대한 정보를 얻고싶을 때
 
 // res.write로 HTML로 보낼 수 있다. 하지만 이런식으로 줄 구분하면 비효율적이다.
 // 서버쪽에 라우터들도 다 쪼갤 필요가 있다.
