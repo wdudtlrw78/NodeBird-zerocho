81bbbe351475bac11a37a98a96141ad747e30c37
momo <wdudtlrw78@gmail.com>
2021-05-04 16:16:10 +0900

    Update back scripts

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -13,14 +13,14 @@ module.exports = {
   test: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'database_test',
+    database: 'react-nodebird',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'database_production',
+    database: 'react-nodebird',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
