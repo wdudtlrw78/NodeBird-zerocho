9993de5392b5e5b34315b57e18c60af3cefb1952
momo <wdudtlrw78@gmail.com>
2021-07-09 22:26:39 +0900

    Fix: infinite scrolling -> intersection oserver

--- prepare/front/pages/user/[id].js
+++ prepare/front/pages/user/[id].js
@@ -4,10 +4,11 @@ import { Avatar, Card } from 'antd';
 import { END } from 'redux-saga';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useInView } from 'react-intersection-observer';
 
 import axios from 'axios';
+import { LOAD_POSTS_REQUEST, LOAD_USER_POSTS_REQUEST } from '../../reducers/post';
 import { LOAD_MY_INFO_REQUEST, LOAD_USER_REQUEST } from '../../reducers/user';
-import { LOAD_USER_POSTS_REQUEST } from '../../reducers/post';
 import PostCard from '../../components/PostCard';
 import wrapper from '../../store/configuerStore';
 import AppLayout from '../../components/AppLayout';
@@ -15,33 +16,22 @@ import AppLayout from '../../components/AppLayout';
 const User = () => {
   const dispatch = useDispatch();
   const router = useRouter();
-
-  // 특정 사용자 id를 받는다.
   const { id } = router.query;
   const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
   const { userInfo, me } = useSelector((state) => state.user);
 
+  const [ref, inView] = useInView();
+
   useEffect(() => {
-    const onScroll = () => {
-      if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
-        if (hasMorePosts && !loadPostsLoading) {
-          // 그 사용자에 대한 게시글만 가져온다.
-          // 페이스북으로 치면 남의 타임라인 들어가는 것
-          dispatch({
-            // LOAD_POSTS_REQUEST 는 모든 게시글들 불러온다면
-            // LOAD_USER_POSTS_REQUEST 는 특정 사용자의 게시글들만 보여준다.
-            type: LOAD_USER_POSTS_REQUEST,
-            lastId: mainPosts[mainPosts.length - 1] && mainPosts[mainPosts.length - 1].id, // 페이지 네이션을 위해서 lastId
-            data: id,
-          });
-        }
-      }
-    };
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, [mainPosts.length, hasMorePosts, id, loadPostsLoading]);
+    if (inView && hasMorePosts && !loadPostsLoading) {
+      const lastId = mainPosts[mainPosts.length - 1]?.id;
+      dispatch({
+        type: LOAD_POSTS_REQUEST,
+        lastId,
+        data: id,
+      });
+    }
+  }, [inView, hasMorePosts, loadPostsLoading, mainPosts, id]);
 
   return (
     <AppLayout>
@@ -54,11 +44,10 @@ const User = () => {
           <meta name="description" content={`${userInfo.nickname}님의 게시글`} />
           <meta property="og:title" content={`${userInfo.nickname}님의 게시글`} />
           <meta property="og:description" content={`${userInfo.nickname}님의 게시글`} />
-          <meta property="og:image" content="https://nodebird.com/favicon.ico" />
-          <meta property="og:url" content={`https://nodebird.com/user/${id}`} />
+          <meta property="og:image" content="https://nodebmomoird.com/favicon.ico" />
+          <meta property="og:url" content={`https://nodemomobird.com/user/${id}`} />
         </Head>
       )}
-      {/* 나의 게시글인경우 null(안뜨게) */}
       {userInfo && userInfo.id !== me?.id ? (
         <Card
           style={{ marginBottom: 20 }}
@@ -83,32 +72,33 @@ const User = () => {
           <Card.Meta avatar={<Avatar>{userInfo.nickname[0]}</Avatar>} title={userInfo.nickname} />
         </Card>
       ) : null}
-      {mainPosts.map((post) => (
-        <PostCard key={post.id} post={post} />
+      {mainPosts.map((c) => (
+        <PostCard key={c.id} post={c} />
       ))}
+      <div ref={hasMorePosts && !loadPostsLoading ? ref : undefined} />
     </AppLayout>
   );
 };
 
-export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-  const cookie = context.req ? context.req.headers.cookie : '';
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req, params }) => {
+  const cookie = req ? req.headers.cookie : '';
   axios.defaults.headers.Cookie = '';
-  if (context.req && cookie) {
+  if (req && cookie) {
     axios.defaults.headers.Cookie = cookie;
   }
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_USER_POSTS_REQUEST,
-    data: context.params.id,
+    data: params.id,
   });
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_MY_INFO_REQUEST,
   });
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_USER_REQUEST,
-    data: context.params.id,
+    data: params.id,
   });
-  context.store.dispatch(END);
-  await context.store.sagaTask.toPromise();
+  store.dispatch(END);
+  await store.sagaTask.toPromise();
 });
 
 export default User;
