a45d9ebfcbc9dc6fadba0f63670f9a9390c09b33
momo <wdudtlrw78@gmail.com>
2021-07-09 22:22:30 +0900

    Fix: nfinite scrolling -> intersection boserver

--- prepare/front/pages/hashtag/[tag].js
+++ prepare/front/pages/hashtag/[tag].js
@@ -1,12 +1,12 @@
 // hashtag/[tag].js
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import { END } from 'redux-saga';
+import { useInView } from 'react-intersection-observer';
 
 import axios from 'axios';
-import { LOAD_HASHTAG_POSTS_REQUEST } from '../../reducers/post';
+import { LOAD_HASHTAG_POSTS_REQUEST, LOAD_POSTS_REQUEST } from '../../reducers/post';
 import PostCard from '../../components/PostCard';
 import wrapper from '../../store/configuerStore';
 import { LOAD_MY_INFO_REQUEST } from '../../reducers/user';
@@ -17,51 +17,44 @@ const Hashtag = () => {
   const router = useRouter();
   const { tag } = router.query;
   const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    const onScroll = () => {
-      if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
-        if (hasMorePosts && !loadPostsLoading) {
-          dispatch({
-            type: LOAD_HASHTAG_POSTS_REQUEST,
-            lastId: mainPosts[mainPosts.length - 1] && mainPosts[mainPosts.length - 1].id,
-            data: tag,
-          });
-        }
-      }
-    };
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, [mainPosts.length, hasMorePosts, tag, loadPostsLoading]);
+    if (inView && hasMorePosts && !loadPostsLoading) {
+      const lastId = mainPosts[mainPosts.length - 1]?.id;
+      dispatch({
+        type: LOAD_POSTS_REQUEST,
+        lastId,
+        data: tag,
+      });
+    }
+  }, [inView, hasMorePosts, loadPostsLoading, mainPosts, tag]);
 
   return (
     <AppLayout>
       {mainPosts.map((c) => (
         <PostCard key={c.id} post={c} />
       ))}
+      <div ref={hasMorePosts && !loadPostsLoading ? ref : undefined} />
     </AppLayout>
   );
 };
 
-export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-  console.log(context);
-  const cookie = context.req ? context.req.headers.cookie : '';
-  console.log(context);
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req, params }) => {
+  const cookie = req ? req.headers.cookie : '';
   axios.defaults.headers.Cookie = '';
-  if (context.req && cookie) {
+  if (req && cookie) {
     axios.defaults.headers.Cookie = cookie;
   }
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_MY_INFO_REQUEST,
   });
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_HASHTAG_POSTS_REQUEST,
-    data: context.params.tag,
+    data: params.tag,
   });
-  context.store.dispatch(END);
-  await context.store.sagaTask.toPromise();
+  store.dispatch(END);
+  await store.sagaTask.toPromise();
 });
 
 export default Hashtag;
